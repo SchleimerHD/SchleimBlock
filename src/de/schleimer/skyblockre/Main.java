53920a72d6b8819d134560c79f2c290d07b242eb
Schleimer <55507303+SchleimerYT@users.noreply.github.com>
2021-05-03 20:49:51 +0200

    V 1.0.1 + Spawn Command + Cleanroom Generator + Teleport to other Islands

--- src/de/schleimer/skyblockre/Main.java
+++ src/de/schleimer/skyblockre/Main.java
@@ -2,9 +2,9 @@ package de.schleimer.skyblockre;
 
 import de.schleimer.skyblockre.api.Generator;
 import de.schleimer.skyblockre.commands.Island;
+import de.schleimer.skyblockre.commands.Spawn;
 import de.schleimer.skyblockre.events.portal;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,6 +19,7 @@ public class Main extends JavaPlugin {
     }
     public boolean initCommands(){
         getCommand("is").setExecutor(new Island());
+        getCommand("spawn").setExecutor(new Spawn());
         return true;
     }
     public boolean registerEvents(){
--- src/de/schleimer/skyblockre/commands/Spawn.java
+++ src/de/schleimer/skyblockre/commands/Spawn.java
@@ -2,6 +2,7 @@ package de.schleimer.skyblockre.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,7 +13,11 @@ public class Spawn implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (commandSender instanceof Player){
             Player p = (Player) commandSender;
-            Location l = new Location(Bukkit.getWorld("world"),0,60,0);
+            World w = Bukkit.getWorld("world");
+            Location l = new Location(w,0.5,65,0.5);
+            l.setYaw(0);
+            l.setPitch(0);
+            w.setSpawnLocation(l.getBlockX(),l.getBlockY(),l.getBlockZ());
             p.teleport(l);
         }
         return false;
